226effcbc16585c1469876279b4b8808beb2fe3f
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-08-17 16:18:07 +0300

    Update social card image in Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/unfurl-optimized.png',
     navbar: {
       title: 'Copy Page Button',
       logo: {
